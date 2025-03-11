cfa4e95ddcfe2e4c0dfd09f83a56fbfdfc4745b6
dezzy <desmond.monotone@gmail.com>
2025-03-11 16:03:55 +0000

    implement Auth context in Home.jsx so that the user cannot see the Dashboard and create resume page until they log in

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import Dashboard from "../components/Dashboard";
-
+import LogIn from "../components/LogIn";
+import { useAuth } from "../context/AuthContext";
 
 const Home = () => {
+    const userAuth = useAuth();
 
     return (
         <div className="home">
-            <Dashboard/>
+            {
+            userAuth.isLoggedIn ?  <Dashboard/> : <LogIn/>
+            }
         </div>
     )
 }
