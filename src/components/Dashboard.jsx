ed771c7eed57039325a4a52057c6e2d43cad5117
dezzy <desmond.monotone@gmail.com>
2025-03-12 16:15:25 +0000

    added Resumes.jsx, added 'logged in as user' line in dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,13 @@
 import CreateResume from "./CreateResume";
+import { useAuth } from "../context/AuthContext";
 
 const Dashboard = () => {
-    
+    const {user, logout} = useAuth();
 
     return (
         <div>
             <h1>Dashboard</h1>
+            <p>logged in as user: {user._id}</p>
             <CreateResume />
         </div>
     );
---
+++ src/components/Resumes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Resumes = () => {
+  return (
+    <div>
+        <h1>Your Resumes</h1>
+        <div className="resume-list max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg">
+
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Resumes
