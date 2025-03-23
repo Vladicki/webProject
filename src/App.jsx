702bdc4ba3b1e7fa9344021a9151e2448e828b4a
HARSH <160406269+7-FLASH-7@users.noreply.github.com>
2025-03-23 12:00:25 +0000

    Updated app.jsx fix some errors

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,28 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
 import Home from './pages/Home';
 import LogIn from './components/LogIn';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 import Resumes from './components/Resumes';
+import Footers from './components/Footers';  // Import Footer
 
 function App() {
-
   return (
     <AuthContextProvider>
       <div className="App">
-        {/* by default authentic routes */}
         <BrowserRouter>
           <Navbar />
           <div className="pages">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/resumes" element={<Resumes />} />
-              <Route path="/login" element={<LogIn/>}/>
+              <Route path="/login" element={<LogIn />} />
             </Routes>
           </div>
+          <Footers /> {/* Add Footer here */}
         </BrowserRouter>
       </div>
     </AuthContextProvider>
-
-  )
+  );
 }
 
-export default App
+export default App;
