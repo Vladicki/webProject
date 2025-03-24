386dcbe2efba293a85e552fdccd3fc2e847310d1
HARSH <160406269+7-FLASH-7@users.noreply.github.com>
2025-03-24 18:15:49 +0000

    remove unwanted things

--- server/server.js
+++ server/server.js
@@ -1,39 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
-// import dotenv from 'dotenv';
-// import { connectDB } from './config/db.js';
-// import User from './models/user.model.js';
-// import userRoutes from './routes/users.js';
-// import resumeRoutes from './routes/resumes.js';
-
-// // import cors from "cors";
- 
-
-// //loads .env file into process.env so that we can use it
-// dotenv.config();
-
-// //create an express app and define the port
-// const app = express();
-// const PORT = 4000;
-
-// app.use(express.json());
-// app.use(cors());
-
-// //app.use(cors({ origin: "http://localhost:3000", credentials: true }));
-
-// // requests ending in /api/users will be directed to user-related functions like sign-up and login
-// app.use('/api/users',userRoutes);
-// //requests ending in /api/resumes will be directed to resume CRUD functions
-// app.use('/api/resumes',resumeRoutes);
-
-// //test command to check that the server's up
-// app.use('/api/ping',(req,res)=>{res.send("Ping! I'm alive!")});
-
-// //start server
-// app.listen(PORT,()=>{
-//     connectDB();
-//     console.log(`Web Project server started at: http://localhost:${PORT}`)
-// })
    
 import express from 'express';
 import cors from 'cors';
