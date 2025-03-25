7b5bf0bf1d91f0b8395a14ebbe0b8c0168480ee7
OYama1909 <125834754+OYama1909@users.noreply.github.com>
2025-03-25 14:40:46 +0000

    footer - link to our github

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -10,22 +10,22 @@ function Footer() {
         </div>
 
         {/* Middle Section - Links */}
-        <div className="flex space-x-6 my-4 md:my-0">
+        {/* <div className="flex space-x-6 my-4 md:my-0">
           <Link to="/" className="hover:text-gray-400 transition">Dashboard</Link>
           <Link to="/resumes" className="hover:text-gray-400 transition">Resumes</Link>
           <Link to="/login" className="hover:text-gray-400 transition">Log In</Link>
-        </div>
+        </div> */}
 
         {/* Right Section - Social Media Icons */}
         <div className="flex space-x-4">
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+          {/* <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
             <i className="fab fa-twitter"></i>
           </a>
           <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
             <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
-            <i className="fab fa-github"></i>
+          </a> */}
+          <a href="https://github.com/Vladicki/webProject" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400">
+            <i className="fab fa-github">Github</i>
           </a>
         </div>
       </div>
